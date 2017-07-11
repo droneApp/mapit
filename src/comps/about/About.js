a611d9702943dd012eaf8f1201a80e736481377d
Dallin Anderson <andersondallin11@gmail.com>
2017-07-11 17:01:05 -0600

    first start on the about page

---
+++ src/comps/about/About.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class App extends Component {
+    render() {
+        return ( 
+            <div>
+                <img />
+                <p>Steen</p>
+                <img />
+                <p>Dallin</p>
+                <img />
+                <p>Jacob</p>
+                <img />
+                <p>Sean</p>
+                
+                </div>
+
+        )
+    }
+}
