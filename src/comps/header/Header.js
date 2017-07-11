e07fff70a4ebcef7b3ff16b30f47f7d334d5b087
Sean <sevensidedsilver@gmail.com>
2017-07-11 15:26:49 -0600

    comp folder

---
+++ src/comps/header/Header.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
+
+
+import './home.css'
+
+export default function Header(){
+  return (
+
+    <header>
+      <nav>
+        
+
+
+
+
+
+
+      </nav>
+    </header>
+
+
+
+
+
+  // end or return
+  )
+
+// end of home
+}
---
+++ src/comps/home/Home.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
+import Header from './comps/header/Header'
+import Footer from './comps/footer/Footer'
+
+import './home.css'
+
+export default function Home(){
+  return (
+
+    <main>
+      <Header />
+
+
+
+
+
+
+      <Footer />
+    </main>
+
+
+
+
+
+  // end or return
+  )
+
+// end of home
+}
