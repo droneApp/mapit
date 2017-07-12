b22e48cf064cf64a6a42592fbea75b16912696a8
sargent88 <steen.sargent@gmail.com>
2017-07-12 10:04:04 -0600

    mapbox issues

---
+++ src/comps/maps/addImage/AddImageMap.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import './addImageMap.css';
+
+export default function AddImageMap() {
+    return (
+        <h1>Satellite View</h1>
+    )
+}
---
+++ src/comps/maps/geoJSON/GeoJSONMap.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import './geoJSONMap.css';
+
+export default function geoJSONMap() {
+    return (
+        <h1>Pick Your Points</h1>
+    )
+}
