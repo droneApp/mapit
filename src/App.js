3efa89ddfed0fa40bb0770e6cb1a1db9fa0e32e3
Dallin Anderson <andersondallin11@gmail.com>
2017-07-12 10:25:00 -0600

    about page set up finnished with pics

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          Dallin was heref <code>src/App.js</code> and save to reload.
+          Dallin was here <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
--- src/comps/about/About.js
+++ src/comps/about/About.js
@@ -1,20 +1,30 @@
 import React from 'react';
-
+import './about.css';
 class App extends Component {
     render() {
         return ( 
-            <div>
-                <img />
-                <p>Steen</p>
-                <img />
-                <p>Dallin</p>
-                <img />
-                <p>Jacob</p>
-                <img />
-                <p>Sean</p>
-                
+           <div>
+            <head>
+            <h2>About Us</h2>
+            </head>
+            <section class="top-row">
+                  <p> 
+                <img class='steen-about-pics' src='./our-pics/16797776_10158254923960300_8611520582224132040_o.jpg' />
+                Steen</p>
+                <p>
+                <img class='about-pics' src='./our-pics/IMG_2076.jpg' />
+                Dallin</p>
+              </section>
+              <section class="bottom-row">
+                <p>
+                <img class='about-pics' src='./our-pics/IMG_0051.JPG' />
+                Jacob</p>
+                <p>
+                <img class='about-pics' src='./our-pics/with-Doge.jpg' />
+                Sean</p>
+              </section>
                 </div>
 
-        )
+        );
     }
 }
