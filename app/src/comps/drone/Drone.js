fd570f0cad2c9752bbfb0a989cf4289888edc947
Jacob Aintablian <jacob.aintablian@gmail.com>
2017-07-13 15:42:22 -0600

    drone

--- app/src/comps/drone/Drone.js
+++ app/src/comps/drone/Drone.js
@@ -5,19 +5,19 @@ class Drone extends Component {
     render(){
         return(
             <div>
-                <div className="diamond"><p className="diamondtext">Strafe Left</p></div>
-                <div className="diamond2"><p className="diamondtext">Move Back</p></div>
-                <div className="diamond3"><p className="diamondtext">Strafe Right</p></div>
-                <div className="diamond4"><p className="diamondtext">Move Forward</p></div>
-                <div className="diamond5"><p className="diamondtext">Turn Left</p></div>
-                <div className="diamond6"><p className="diamondtext">Lower Drone</p></div>
-                <div className="diamond7"><p className="diamondtext">Turn Right</p></div>
-                <div className="diamond8"><p className="diamondtext">Rise Drone</p></div>
-                <div className="">Scope out</div>
-                <div className="">Scope in</div>
-                <div className="">Take Photo</div>
-                <div className="">Create Destination</div>
-                <div className="">Auto-Pilot to Destination</div>
+                <div className="diamond"><p className="diamondtext">&larr;</p></div>
+                <div className="diamond2"><p className="diamondtext">&larr;</p></div>
+                <div className="diamond3"><p className="diamondtext">&larr;</p></div>
+                <div className="diamond4"><p className="diamondtext">&larr;</p></div>
+                <div className="diamond5"><img className="counterclock" src={require("./assets/counterclock.png")} alt="Turn Left" /></div>
+                <div className="diamond6"><p className="down">DOWN</p><p className="downarrow">&#8897;</p></div>
+                <div className="diamond7"><img className="clock" src={require("./assets/clockwise.png")} alt="Turn Left" /></div>
+                <div className="diamond8"><p className="uparrow">&#8896;</p><p className="up">UP</p></div>
+                <div className="scope">+</div>
+                <div className="scopeout">-</div>
+                <div className="photo">Take Photo</div>
+                <div className="destination">Create Destination</div>
+                <div className="autopilot">Auto-Pilot to Destination</div>
                 <div className="depart">Take-Off</div>
                 <div className="land">Land</div>
             </div>
