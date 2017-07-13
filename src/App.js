cc5a8121add54ef8803c30dc6140cc716d4268c2
Dallin Anderson <andersondallin11@gmail.com>
2017-07-13 09:10:58 -0600

    routing set up started

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Switch, HashRouter, Route} from 'react-router-dom';
+import Home from './comps/home/Home';
+import Team from './comps/team/Team';
+import Tech from './comps/tech/Tech';
 
 class App extends Component {
   render() {
     return (
       <div>
-      {this.props.children}
+        <HashRouter>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route path="/team" component={Team}/>
+        <Route path="/tech" component={Tech}/>
+      </Switch>
+      </HashRouter>
       </div>
      
 
