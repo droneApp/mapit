a2a4b69ff025144f13087448fcfdca59e1866dbf
sargent88 <steen.sargent@gmail.com>
2017-07-14 11:04:13 -0600

    google maps now

--- app/src/comps/maps/geoJSON/GeoJSONMap.js
+++ app/src/comps/maps/geoJSON/GeoJSONMap.js
@@ -1,44 +1,44 @@
 import React, {Component} from 'react';
 import './geoJSONMap.css';
-import ReactMapboxGl, {Layer, GeoJSONLayer} from 'react-mapbox-gl';
+import {withGoogleMap, GoogleMap, Rectangle, DrawingManager} from 'react-google-maps';
 
-const Map = ReactMapboxGl({
-    accessToken: 'pk.eyJ1Ijoic2FyZ2VudDg4IiwiYSI6ImNqMzY0aHRoODAwbGszMmxpdjd5NTl6OHgifQ.zWE7w8Bs3NvC6rhFXguUTQ'
-})
+const GettingStartedGoogleMap = withGoogleMap(props => (
+  <GoogleMap
+    ref={props.onMapLoad}
+    defaultZoom={3}
+    defaultCenter={{ lat: 40.2263, lng: 111.6608 }}
+    onClick={props.onMapClick}
+  >
+    {props.markers.map((marker, index) => (
+      <Marker
+        {...marker}
+        onRightClick={() => props.onMarkerRightClick(index)}
+      />
+    ))}
+  </GoogleMap>
+));
 
-<Map
-    container = 'map'
-    style = 'mapbox://styles/mapbox/streets-v9'
-    center = {[111.6608, 40.2263]}
-    className = 'geo-map'
-    containerStyle={{
-        height: "100vh",
-        width: "100vw"
-  }}>
-    <Layer 
-        layout = {{}}
-        paint = {{
-            fill-color: '#088',
-            fill-opacity: 0.8
-        }}>
-        <GeoJSON
-            type = 'fill'
-            source = {{
-                type: 'geojson',
-                data: {
-                    type: 'Feature',
-                    geometry: {
-                        type: 'Polygon',
-                        coordinates: location
-                    }
-                }
-            }}/>
-    </Layer>
-</Map>
+render(
+  <GettingStartedGoogleMap
+    containerElement={
+      <div style={{ height: `100%` }} />
+    }
+    mapElement={
+      <div style={{ height: `100%` }} />
+    }
+    onMapLoad={_.noop}
+    onMapClick={_.noop}
+    markers={markers}
+    onMarkerRightClick={_.noop}
+  />,
+  document.getElementById('root')
+);
+
 
 
 export default function geoJSONMap() {
     return (
         <h1>Pick Your Points</h1>
+        <GettingStartedGoogleMap />
     )
 }
